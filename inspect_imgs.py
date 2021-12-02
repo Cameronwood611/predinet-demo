6d39f2cc838e65c76d00fd1836f0ddf9e83af5b8
Cameronwood611 <cwood611@uab.edu>
2021-12-02 11:19:14 -0600

    Look at test data img for predinet

---
+++ inspect_imgs.py
@@ -0,0 +1,17 @@
+
+# PIP
+import numpy as np
+
+
+def main():
+    loader = np.load('mini_3task_col_patts_stripes.npz', 'rb')
+    print(loader)
+    images = loader['images']
+    labels = loader['labels']
+    tasks = loader['tasks']
+    print(labels)
+    print(tasks)
+
+
+if __name__ == "__main__":
+    main()
